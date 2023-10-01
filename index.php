7fafe148865f011e58c93ef48f28a35e6d1be2fc
unknown <nkhub2018@agruni.edu.ge>
2023-10-02 00:13:31 +0400

    Changed files

--- index.php
+++ index.php
@@ -491,53 +491,49 @@
   <!-- client section -->
   <section class="client_section layout_padding">
     <div class="container">
-      <div class="heading_container">
-        <h2>
-          <span>Testimonial</span>
-        </h2>
-      </div>
+        <div class="heading_container">
+            <h2>
+                <span>Testimonial</span>
+            </h2>
+        </div>
     </div>
     <div class="container px-0">
-      <div id="customCarousel2" class="carousel  carousel-fade" data-ride="carousel">
-        <div class="carousel-inner">
-        
-          <?php 
-              foreach ($sections as $section) {
-                echo ' <div class="carousel-item active">
-                <div class="box">
-                  <div class="client_info">
-                    <div class="client_name">
-                      <h5>
-                        '. $section['h1'] .'
-                      </h5>
-                      <h6>
-                        Default model text
-                      </h6>
+        <div id="customCarousel2" class="carousel carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+                <?php foreach ($sections as $index => $section) : ?>
+                    <div class="carousel-item <?php echo $index === 0 ? 'active' : ''; ?>">
+                        <div class="box">
+                            <div class="client_info">
+                                <div class="client_name">
+                                    <h5>
+                                        <?php echo $section['h1']; ?>
+                                    </h5>
+                                    <h6>
+                                        Default model text
+                                    </h6>
+                                </div>
+                                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            </div>
+                            <p>
+                                <?php echo $section['text']; ?>
+                            </p>
+                        </div>
                     </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
-                  </div>
-                  <p>
-                    '. $section['text'] .'
-                  </p>
-                </div>
-                </div>';
-              }
-          ?>
-          
+                <?php endforeach; ?>
+            </div>
+            <div class="carousel_btn-box">
+                <a class="carousel-control-prev" href="#customCarousel2" role="button" data-slide="prev">
+                    <i class="fa fa-angle-left" aria-hidden="true"></i>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#customCarousel2" role="button" data-slide="next">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
-        <div class="carousel_btn-box">
-          <a class="carousel-control-prev" href="#customCarousel2" role="button" data-slide="prev">
-            <i class="fa fa-angle-left" aria-hidden="true"></i>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#customCarousel2" role="button" data-slide="next">
-            <i class="fa fa-angle-right" aria-hidden="true"></i>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
   <!-- end client section -->
 
   <!-- contact section -->
